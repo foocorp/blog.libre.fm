00e1d8f64b10c95e74ac6b7fbd1147020aaad1f7
Dr. Matt Lee <mattl@cnuk.org>
2024-10-29 18:16:45 -0400

    Scrappy libre.fm modifications

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
-const cleanCSS = require("clean-css");
+// const cleanCSS = require("clean-css");
 const fs = require("fs");
 const pluginRSS = require("@11ty/eleventy-plugin-rss");
-const localImages = require("eleventy-plugin-local-images");
-const lazyImages = require("eleventy-plugin-lazyimages");
+// const localImages = require("eleventy-plugin-local-images");
+// const lazyImages = require("eleventy-plugin-lazyimages");
 const ghostContentAPI = require("@tryghost/content-api");
 
-const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+// const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
 // Init Ghost API
 const api = new ghostContentAPI({
@@ -23,29 +23,29 @@ const stripDomain = url => {
 
 module.exports = function(config) {
   // Minify HTML
-  config.addTransform("htmlmin", htmlMinTransform);
+  // config.addTransform("htmlmin", htmlMinTransform);
 
   // Assist RSS feed template
   config.addPlugin(pluginRSS);
 
   // Apply performance attributes to images
-  config.addPlugin(lazyImages, {
-    cacheFile: ""
-  });
+  //config.addPlugin(lazyImages, {
+    //cacheFile: ""
+	  //});
 
   // Copy images over from Ghost
-  config.addPlugin(localImages, {
-    distPath: "dist",
-    assetPath: "/assets/images",
-    selector: "img",
-    attribute: "data-src", // Lazy images attribute
-    verbose: false
-  });
+  // config.addPlugin(localImages, {
+//     distPath: "dist",
+//     assetPath: "/assets/images",
+//     selector: "img",
+//     attribute: "data-src", // Lazy images attribute
+//     verbose: false
+//   });
 
   // Inline CSS
-  config.addFilter("cssmin", code => {
-    return new cleanCSS({}).minify(code).styles;
-  });
+  //config.addFilter("cssmin", code => {
+    //return new cleanCSS({}).minify(code).styles;
+  //});
 
   config.addFilter("getReadingTime", text => {
     const wordsPerMinute = 200;
@@ -84,6 +84,8 @@ module.exports = function(config) {
 
     return collection;
   });
+  
+  
 
   // Get all posts
   config.addCollection("posts", async function(collection) {
@@ -97,12 +99,22 @@ module.exports = function(config) {
       });
 
     collection.forEach(post => {
-      post.url = stripDomain(post.url);
       post.primary_author.url = stripDomain(post.primary_author.url);
       post.tags.map(tag => (tag.url = stripDomain(tag.url)));
 
       // Convert publish date into a Date object
       post.published_at = new Date(post.published_at);
+	  
+	  var MyDate = post.published_at;
+	  var MyDateString;
+
+	  MyDate.setDate(MyDate.getDate());
+
+	  MyDateString = MyDate.getFullYear() + "/" + ('0' + (MyDate.getMonth() + 1)).slice(-2);
+	  
+      post.url = "/" + MyDateString + stripDomain(post.url);
+	  
+	  
     });
 
     // Bring featured post to the top of the list
@@ -111,89 +123,89 @@ module.exports = function(config) {
     return collection;
   });
 
-  // Get all authors
-  config.addCollection("authors", async function(collection) {
-    collection = await api.authors
-      .browse({
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Get all posts with their authors attached
-    const posts = await api.posts
-      .browse({
-        include: "authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Attach posts to their respective authors
-    collection.forEach(async author => {
-      const authorsPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_author.id === author.id;
-      });
-      if (authorsPosts.length) author.posts = authorsPosts;
-
-      author.url = stripDomain(author.url);
-    });
-
-    return collection;
-  });
+  // // Get all authors
+  // config.addCollection("authors", async function(collection) {
+  //   collection = await api.authors
+  //     .browse({
+  //       limit: "all"
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  //
+  //   // Get all posts with their authors attached
+  //   const posts = await api.posts
+  //     .browse({
+  //       include: "authors",
+  //       limit: "all"
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  //
+  //   // Attach posts to their respective authors
+  //   collection.forEach(async author => {
+  //     const authorsPosts = posts.filter(post => {
+  //       post.url = stripDomain(post.url);
+  //       return post.primary_author.id === author.id;
+  //     });
+  //     if (authorsPosts.length) author.posts = authorsPosts;
+  //
+  //     author.url = stripDomain(author.url);
+  //   });
+  //
+  //   return collection;
+  // });
 
   // Get all tags
-  config.addCollection("tags", async function(collection) {
-    collection = await api.tags
-      .browse({
-        include: "count.posts",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Get all posts with their tags attached
-    const posts = await api.posts
-      .browse({
-        include: "tags,authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Attach posts to their respective tags
-    collection.forEach(async tag => {
-      const taggedPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_tag && post.primary_tag.slug === tag.slug;
-      });
-      if (taggedPosts.length) tag.posts = taggedPosts;
-
-      tag.url = stripDomain(tag.url);
-    });
-
-    return collection;
-  });
+  // config.addCollection("tags", async function(collection) {
+ //    collection = await api.tags
+ //      .browse({
+ //        include: "count.posts",
+ //        limit: "all"
+ //      })
+ //      .catch(err => {
+ //        console.error(err);
+ //      });
+ //
+ //    // Get all posts with their tags attached
+ //    // const posts = await api.posts
+ //    //   .browse({
+ //    //     include: "tags,authors",
+ //    //     limit: "all"
+ //    //   })
+ //    //   .catch(err => {
+ //    //     console.error(err);
+ //    //   });
+ //
+ //    // Attach posts to their respective tags
+ //    // collection.forEach(async tag => {
+ //    //   const taggedPosts = posts.filter(post => {
+ //    //     post.url = stripDomain(post.url);
+ //    //     return post.primary_tag && post.primary_tag.slug === tag.slug;
+ //    //   });
+ //    //   if (taggedPosts.length) tag.posts = taggedPosts;
+ //    //
+ //    //   tag.url = stripDomain(tag.url);
+ //    // });
+ //
+ //    return collection;
+ //  });
 
   // Display 404 page in BrowserSnyc
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: (err, bs) => {
-        const content_404 = fs.readFileSync("dist/404.html");
-
-        bs.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+  // config.setBrowserSyncConfig({
+  //   callbacks: {
+  //     ready: (err, bs) => {
+  //       const content_404 = fs.readFileSync("dist/404.html");
+  //
+  //       bs.addMiddleware("*", (req, res) => {
+  //         // Provides the 404 content without redirect.
+  //         res.write(content_404);
+  //         res.end();
+  //       });
+  //     }
+  //   }
+  // });
 
   // Eleventy configuration
   return {
